14a9873040496dc319620ae4dbacb97ca9f852dc
will you ?? <thilak8pro@gmail.com>
2026-06-15 23:55:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const messages = [
+    "awwwww!!!!",
+    "Awwwwwwww!!!!",
+    "Awwwwwwwwww!!!",
+    "Awwwwwwwwwwwwww!!!",
+    "Awwwwwwwwwwwwwww!!!",
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
